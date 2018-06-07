1d2fe2aeed2e00e0b5c0e34359501633359ec247
Nyssa Keller <nyssa1@gmail.com>
2018-06-07 13:26:03 -0600

    Create Agents component

---
+++ frontend/src/Agents/Agents.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Agents.css';
+
+class Agents extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+};
+
+export default Agents;
