3c512e730f49f0d50e16fc98f34d9c5e6824a793
Nyssa Keller <nyssa1@gmail.com>
2018-06-07 12:26:22 -0600

    Create Home component and add city background image

---
+++ frontend/src/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className='home'></div>
+    );
+  }
+};
+
+export default Home;
